2a8f951b5932954935707a789e141b6f97055e60
Ashwin Phadnis <ashwin.phadnis@gmail.com>
2019-02-13 12:19:13 -0800

    Authorization implemented

--- public/javascripts/articleApp.js
+++ public/javascripts/articleApp.js
@@ -36,12 +36,14 @@ app.config(function($routeProvider) {
 
 // Create a factory of services to get all articles from Mongo DB (routes/api.js)
 app.factory('articleService', function($resource) {
-    return $resource('/api/articles');
+    return $resource('/api/articles/:articleId', {articleId: '@id'},
+        {update: {method:'PUT'}});
 });
 
-app.controller('articleController', function($scope, articleService) {
+app.controller('articleController', function($scope, articleService, $location, $rootScope) {
     // Create an articles array to store all articles
     $scope.articles = [];
+    $scope.editMode = false; // Variable to store if existing article is edited or new article is posted
 
     // Call articleservice and show all articles
     $scope.articles = articleService.query();
@@ -49,12 +51,67 @@ app.controller('articleController', function($scope, articleService) {
     // Call Post service (routes/ api.js) and show information
     $scope.post = function() {
         $scope.newArticle.timeStamp = Date.now(); // Use the current date/time while inserting record
-        
+        $scope.newArticle.username = $rootScope.currentUser;    // Get the name of the user from global variable
+
         articleService.save($scope.newArticle, function(res){
-            $scope.articles = articleService.query();   // Get all articles
-            // Reset values on screen for user
-            $scope.newArticle = {username: '', title: '', text: '', timeStamp:''};
+            // Check if the user is authenticated, only then allow saving of article
+            // Else, redirect to sign in page
+            if (res.status == 'Authentication failure') {
+                $location.path('/signin');
+            } else {
+                $scope.articles = articleService.query();   // Get all articles
+                // Reset values on screen for user
+                $scope.newArticle = {username: '', title: '', text: '', timeStamp:''};
+            }
         });
+    }
+
+    // Edit button clicked on existing article
+    $scope.edit = function(article) {
+        $scope.editMode = true;
+        $scope.newArticle = articleService.get({articleId: article._id});
+    }
+
+    // Update button clicked for saving existing article
+    $scope.update = function(article) {
+        $scope.newArticle.timeStamp = Date.now(); // Use the current date/time while updating record
+        
+        articleService.update($scope.newArticle, function(res){
+        // Check if the user is authenticated, only then allow saving of article
+        // Else, redirect to sign in page
+            if (res.status == 'Authentication failure') {
+                $location.path('/signin');
+            }
+            else {
+                $scope.articles = articleService.query();   // Get all articles
+                // Reset values on screen for user
+                $scope.newArticle = {username: '', title: '', text: '', timeStamp:''};
+            }
+            $scope.editMode = false;
+        });
+    }
+
+    // Delete button clicked
+    $scope.delete = function(article) {
+        if(confirm('Are you sure you want to delete this article?')) {
+            // Check if the user is authenticated, only then allow saving of article
+            // Else, redirect to sign in page
+            articleService.delete({articleId: article._id}, function(res) {
+                if (res.status == 'Authentication failure') {
+                    $location.path = '/signin';
+                }
+                else {
+                    $scope.articles = articleService.query();// Get all articles
+                    // Reset values on screen for user
+                    $scope.newArticle = {username: '', title: '', text: '', timeStamp: ''};
+                }
+            });
+        }
+    }
+
+    // Check ownership of article
+    $scope.isUserOwner = function(article) {
+        return (article.username == $rootScope.currentUser);
     }
 });
 
--- routes/api.js
+++ routes/api.js
@@ -2,6 +2,22 @@ const express = require('express');
 const router = express.Router();
 const Article = require('./../models/articles');    // Model to interact with mongo DB
 
+// Intercepts all requests of  '/articles' route
+// Checks if user is authenticated before allowing to post
+// Get requests are allowed
+router.use('/articles', function(req, res, next) {
+    console.log(req.method + "<<<" + req.isAuthenticated());
+    if(req.method == 'GET') {
+        return next();
+    }
+    else if (req.isAuthenticated()) {
+        return next();
+    }
+    else {
+        res.send({status: 'Authentication failure'});
+    }
+});
+
 // Get all articles from Mongo DB
 router.get ('/articles', function(req, res, next) {
     Article.find (function(err, articles) {
@@ -27,6 +43,31 @@ router.post ('/articles', function(req, res, next) {
     newArticle.save(function(err, article) {
         if (err) return res.send(500, err);
         return res.json(article);
+    });
+});
+
+// Middleware function that handles the article delete requests
+router.delete ('/articles/:id', function(req, res, next) {
+    Article.remove({_id: req.params.id}, function(err, article) {
+        if (err) return res.send(err);
+        res.json({message: 'Successfully deleted'});
+    });
+});
+
+// Middleware function that updates the articles
+router.put ('/articles', function(req, res, next) {
+    Article.findOne({_id: req.body._id}, function(err, article) {
+        if(err) {
+            return res.send(err);
+        }
+        article.username = req.body.username;
+        article.title = req.body.title;
+        article.text = req.body.text;
+        article.timeStamp = req.body.timeStamp;
+        article.save(function(err, article) {
+            if (err) return res.send(500, err);
+            res.json({message: 'Article Updated'});
+        });
     });
 });
 
